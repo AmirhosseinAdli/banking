5192f667c91f69214c429c42474042aa4b82fcc4
a.adli <amirhossein.adli@gmail.com>
2022-03-28 11:09:54 +0430

    added two apis route to app.go added gorilla github repository

--- app/app.go
+++ app/app.go
@@ -1,18 +1,32 @@
 package app
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func Start() {
 
-	mux := http.NewServeMux()
+	//mux := http.NewServeMux()
+	router := mux.NewRouter()
 
 	// define routes
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/api/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/api/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/api/customers", createCustomer).Methods(http.MethodPost)
+	router.HandleFunc("/api/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	// starting servers
-	log.Fatal(http.ListenAndServe("127.0.0.1:2000", mux))
+	log.Fatal(http.ListenAndServe("127.0.0.1:2000", router))
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post request received")
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, vars["customer_id"])
 }
